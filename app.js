6d349ac91a0c999c4e5c99485706a0a475e20aa3
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-03-28 14:33:38 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-/**
- * React Static Boilerplate
- * https://github.com/koistya/react-static-boilerplate
- * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
- */
 
-import 'babel/polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
-import Location from './lib/Location';
-import Layout from './components/Layout';
+import 'babel/polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment'
+import Location from './lib/Location'
+import Layout from './components/Layout.jsx'
 
-const routes = {}; // Auto-generated on build. See tools/lib/routes-loader.js
+const routes = {} // Auto-generated on build. See tools/lib/routes-loader.js
 
 const route = async (path, callback) => {
-  const handler = routes[path] || routes['/404'];
-  const component = await handler();
-  await callback(<Layout>{React.createElement(component)}</Layout>);
-};
+  const handler = routes[path] || routes['/404']
+  const component = await handler()
+  await callback(<Layout>{React.createElement(component)}</Layout>)
+}
 
 function run() {
   const container = document.getElementById('app');
   Location.listen(location => {
     route(location.pathname, async (component) => ReactDOM.render(component, container, () => {
       // Track the page view event via Google Analytics
-      window.ga('send', 'pageview');
-    }));
-  });
+      window.ga('send', 'pageview')
+    }))
+  })
 }
 
 if (canUseDOM) {
@@ -39,4 +34,4 @@ if (canUseDOM) {
   }
 }
 
-export default { route, routes };
+export default { route, routes }
