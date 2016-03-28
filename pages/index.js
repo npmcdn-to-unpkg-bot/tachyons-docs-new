b0b836561e5113f2b70ea94b0f06ea421c08407d
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-03-28 15:15:23 -0400

    principles v1

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import Hero from './Home/Hero.jsx'
 import GettingStarted from './Home/GettingStarted.jsx'
+import Principles from './Home/Principles.jsx'
 
 export default class HomePage extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class HomePage extends Component {
       <section className='w-100 pv5 pv6-ns bt b--black-10 bg-black-0125'>
         <Hero />
         <GettingStarted />
+        <Principles />
       </section>
     )
   }
